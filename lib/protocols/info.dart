1ae2ca8a3010af0976c88644f7a3b7f00f52a028
Craig <craigoda@gmail.com>
2020-07-10 17:53:00 -0700

    changed getInfo to use http package

--- lib/protocols/info.dart
+++ lib/protocols/info.dart
@@ -1,23 +1,15 @@
+import 'package:http/http.dart' as http;
 import 'dart:io';
 import 'dart:convert';
 import 'package:apitest/pretty_print.dart';
 
 void getInfo() async {
-
-  
   // test data to return a single JSON objects from Internet
   // String url = 'https://swapi.co/api/people/1';
   // String url = 'https://jsonplaceholder.typicode.com/users/1';
 
   String url = 'http://192.168.1.1/osc/info';
 
-  var request = await HttpClient().getUrl(Uri.parse(url));
-  // sends the request
-  var response = await request.close(); 
-
-  // transforms and prints the response
-  await for (var contents in response.transform(Utf8Decoder())) {
-    prettyPrint(contents.toString());
-  }
-
+  var response = await http.get(url);
+  prettyPrint(response.body);
 }
